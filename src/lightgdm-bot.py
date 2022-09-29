0720f25a8fdfb4118ab397ab0f8bcfcd83e24ae6
Justin Guese <guese.justin@gmail.com>
2022-09-29 17:11:51 +0200

    lightgbm trend

--- src/lightgdm-bot.py
+++ src/lightgdm-bot.py
@@ -1,12 +1,14 @@
-from basebot import BaseBot
-from datetime import date
-import pandas as pd
-from tqdm import tqdm
-import numpy as np
 import json
+from datetime import date
 from os import environ
+
 import lightgbm as lgb
+import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+
+from basebot import BaseBot
 
 
 class LGBBot(BaseBot):
@@ -17,13 +19,13 @@ class LGBBot(BaseBot):
     def prepareData(self, df):
         df["pctChange"] = df["SMA_3"].pct_change()
         df["signal"] = np.sign(df["pctChange"])
-        df["signal"] = df["signal"].shift(-1)
+        df["signal"] = self.getTrend(df)
         df = df.fillna(0)
         y = df["signal"]
         df = df.drop(["signal", "ticker"], axis = 1)
         # del df
         x_train, x_test, y_train, y_test = train_test_split(
-            df, y, test_size=0.2)
+            df, y, test_size=0.05, shuffle = True)
         train_data = lgb.Dataset(x_train, label = y_train)
         validation_data = lgb.Dataset(x_test, label=y_test, reference=train_data)
         return df, train_data, validation_data, x_test, y_test
@@ -51,38 +53,45 @@ class LGBBot(BaseBot):
         return bst, y_pred, y_pred_soft
 
     
-    def backtest(self):
-        # df = self.getData(self.stock, start_date = date(2019, 1, 1), end_date = date.today(), technical_indicators = ["all"])
-        # print(df.head())
-        # df.to_parquet("tmpaapldata.parquet")
-        df = pd.read_parquet("tmpaapldata.parquet")
+    def backtest(self, iterations = 100, ):
+        df = self.getData(self.stock, start_date = date(2020, 1, 1), end_date = date.today(), technical_indicators = ["all"])
+        print(df.head())
+        df.to_parquet("tmpaapldata.parquet")
+        # df = pd.read_parquet("tmpaapldata.parquet")
         df, train_data, validation_data, x_test, y_test = self.prepareData(df)
         # training
-        bst, y_pred, y_pred_soft = self.train(train_data, validation_data, x_test, y_test)
+        bst, y_pred, y_pred_soft = self.train(train_data, validation_data, x_test, y_test, num_rounds = iterations)
         # now simulate
         startMoney = 10000
-        money = startMoney
-        shares = 0
-        baselineStocks = money / df.iloc[0]["close"]
-        baselineWin = baselineStocks * df.iloc[-1]["close"]
-        for i in range(len(df)):
-            datasnippet = df.iloc[i]
-            pred = bst.predict([datasnippet])[0]
-            pred = self.getDecision(pred)
-            pred_raw = self.getRawDecision(pred)
-            
-            if pred == 1 and money > 0 and shares == 0:
-                shares = money / datasnippet["close"]
-                money = 0
-            elif pred == -1 and shares > 0:
-                money = shares * datasnippet["close"]
-                shares = 0
-            
-        # at the end sell all shares
-        if shares > 0:
-            money += shares * datasnippet["close"]
-        print("money: ", money)
-        print("money per month", money / len(df) / 30)
+        
+        baselineStocks = startMoney / df.iloc[0]["adj_close"]
+        baselineWin = baselineStocks * df.iloc[-1]["adj_close"]
+        
+        bestLookback = -1
+        bestWin = -99999999
+        for lookback in [1, 3, 5, 10, 50]:
+            money = startMoney
+            shares = 0
+            for i in range(lookback, len(df)):
+                datasnippet = df.iloc[i-lookback:i]
+                pred = np.median(bst.predict(datasnippet))
+                pred = self.getDecision(pred)
+                pred_raw = self.getRawDecision(pred)
+                lastPrice = df.iloc[i]["adj_close"]
+                if pred == 1 and money > 0 and shares == 0:
+                    shares = money / lastPrice * (1.00025)
+                    money = 0
+                elif pred == -1 and shares > 0:
+                    money = shares * lastPrice * (1-0.00025)
+                    shares = 0
+            # at the end sell all shares
+            if shares > 0:
+                money += shares * lastPrice
+            if money > bestWin:
+                bestWin = money
+                bestLookback = lookback
+        print("money with lookback %d is %.2f$" % (bestLookback, bestWin))
+        print("money per month", bestWin / len(df) / 30)
         print("baseline money: ", baselineWin)
         # pred
         # accuracy:  0.0
@@ -90,21 +99,14 @@ class LGBBot(BaseBot):
         # money per month 2.4083007141207347
         # baseline money:  38594.22413472413
 
-
-        
-        
-
-
-
-
-    def run(self):
+    def run(self, lookback = 3):
         df = self.getData(self.stock, technical_indicators = ["all"])
         # df = pd.read_parquet("tmpaapldata.parquet")
         df, train_data, validation_data, x_test, y_test = self.prepareData(df)
         # training
         bst, y_pred, y_pred_soft = self.train(train_data, validation_data, x_test, y_test, num_rounds = 100)
         # get last data and preds
-        datasnippet = df.iloc[-3:]
+        datasnippet = df.iloc[-lookback:]
         pred = bst.predict(datasnippet)
         pred = np.sign(np.median(pred))
         # print("i predict. ", pred)
@@ -120,6 +122,8 @@ class LGBBot(BaseBot):
                 print("nothing to do")
 
 if __name__ == "__main__":
-    bot = LGBBot("lgb-bot-" + environ["TICKER"], environ["TICKER"]) # , backendurl="http://tradingbot-baseimage-service:8000"
-    # winOverHolding = bot.backtest()
-    bot.run()
+    environ["TICKER"] = "AAPL"
+    
+    bot = LGBBot("lgb-trend-bot-" + environ["TICKER"], environ["TICKER"]) # , backendurl="http://tradingbot-baseimage-service:8000"
+    bot.backtest()
+    # bot.run()
